d4fa72612a1f392ac9033c46b153bef448e81d71
ReiMontt <sdizon@up.edu.ph>
2025-01-29 02:43:25 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://reimontt.github.io/shopping-site/', // Set base to your repository name
+  base: './', // Set base to your repository name
 })
